82dd5dabbe231ea3330bdddaf8bc1413d7e54e05
Járlei Rodrigues <jarlei_rodrigues@outlook.com>
2024-10-13 11:20:12 -0300

    feat: added insert natural person

--- src/models/sqlite/repositories/natural_person_repository.py
+++ src/models/sqlite/repositories/natural_person_repository.py
@@ -1,10 +1,39 @@
+from dataclasses import dataclass
 from typing import List
 from sqlalchemy.orm.exc import NoResultFound
 from src.models.sqlite.entities.natural_person import NaturalPersonTable
 
+@dataclass
+class NaturalPersonData:
+    renda_mensal: float
+    idade: int
+    nome_completo: str
+    celular: str
+    email: str
+    categoria: str
+    saldo: float
+
 class NaturalPersonRepository:
     def __init__(self, db_connection) -> None:
         self.__db_connection = db_connection
+
+    def insert_natural_person(self, person_data: NaturalPersonData) -> None:
+        with self.__db_connection as database:
+            try:
+                natural_person_data = NaturalPersonTable(
+                    renda_mensal=person_data.renda_mensal,
+                    idade=person_data.idade,
+                    nome_completo=person_data.nome_completo,
+                    celular=person_data.celular,
+                    email=person_data.email,
+                    categoria=person_data.categoria,
+                    saldo=person_data.saldo
+                )
+                database.session.add(natural_person_data)
+                database.session.commit()
+            except Exception as exception:
+                database.session.rollback()
+                raise exception
 
     def list_natural_person(self) -> List[NaturalPersonTable]:
         with self.__db_connection as database:
--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -1,6 +1,6 @@
 import pytest
-from src.models.sqlite.repositories.natural_person_repository import NaturalPersonRepository
 from src.models.sqlite.settings.connection import db_connection_handler
+from .natural_person_repository import NaturalPersonData, NaturalPersonRepository
 from .legal_entity_repository import LegalEntityData, LegalEntityRepository
 
 # db_connection_handler.connect_to_db()
@@ -33,3 +33,18 @@ def test_insert_legal_entity():
 
     repo = LegalEntityRepository(db_connection_handler)
     repo.insert_legal_entity(mock_legal_entity_data)
+
+@pytest.mark.skip(reason="db integration")
+def test_insert_natural_person():
+    mock_natural_person_data = NaturalPersonData(
+        renda_mensal=2850.00,
+        idade=32,
+        nome_completo="nome completo",
+        celular="1111-5555",
+        email="naturalperson@email.com",
+        categoria="category B",
+        saldo=17582.21,
+    )
+
+    repo = NaturalPersonRepository(db_connection_handler)
+    repo.insert_natural_person(mock_natural_person_data)
